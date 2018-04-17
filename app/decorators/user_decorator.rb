40dc163bb7897ec92b0c11a182ae6b2cb96279be
Maciorex <maciej.zabski@gmail.com>
2018-04-17 18:47:30 +0200

    add user helper

--- app/decorators/user_decorator.rb
+++
@@ -1,8 +0,0 @@
-class UserDecorator < Draper::Decorator
-  delegate_all
-
-  def full_name
-    return "Anonymous" unless object.first_name || object.second_name
-    "#{object.first_name} #{object.second_name}"
-  end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  include UserHelper
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
